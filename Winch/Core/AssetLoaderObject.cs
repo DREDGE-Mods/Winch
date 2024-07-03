534644eafa8580771905bb7ec0f2286e94a6b7db
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-07-03 10:36:45 -0400

    AssetLoaderObject debug

--- Winch/Core/AssetLoaderObject.cs
+++ Winch/Core/AssetLoaderObject.cs
@@ -1,16 +1,42 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using Winch.Core.API;
 
 namespace Winch.Core
 {
     internal class AssetLoaderObject : MonoBehaviour
     {
+        private void Awake()
+        {
+            WinchCore.Log.Debug("[AssetLoaderObject] Awake()");
+            GameManager.Instance.OnGameStarted += OnGameStarted;
+            GameManager.Instance.OnGameEnded += OnGameEnded;
+        }
+
+        private void OnDisable()
+        {
+            WinchCore.Log.Debug("[AssetLoaderObject] OnDisable()");
+            GameManager.Instance.OnGameStarted -= OnGameStarted;
+            GameManager.Instance.OnGameEnded -= OnGameEnded;
+        }
+
         public void Start()
         {
+            WinchCore.Log.Debug("[AssetLoaderObject] Start()");
             AssetLoader.LoadAssets();
             DredgeEvent.TriggerModAssetsLoaded();
             ModAssemblyLoader.ExecuteModAssemblies();
             Initializer.InitializePostUnityLoad();
+        }
+
+        private void OnGameStarted()
+        {
+            WinchCore.Log.Debug("[AssetLoaderObject] OnGameStarted()");
+        }
+
+        private void OnGameEnded()
+        {
+            WinchCore.Log.Debug("[AssetLoaderObject] OnGameEnded()");
         }
     }
 }
