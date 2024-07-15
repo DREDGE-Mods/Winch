a96fd0382ca6fa2c91c84fc2a546c2734badc5a7
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-07-14 21:03:37 -0400

    Make the sprites more like DREDGE's formatting

--- Winch/Util/TextureUtil.cs
+++ Winch/Util/TextureUtil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Winch.Core;
 
 namespace Winch.Util
@@ -9,6 +10,7 @@ namespace Winch.Util
     public static class TextureUtil
     {
         private static Dictionary<string, Texture2D> TextureMap = new();
+        private static Dictionary<string, Sprite> SpriteMap = new();
 
         public static Texture2D? GetTexture(string key)
         {
@@ -20,23 +22,34 @@ namespace Winch.Util
 
         public static Sprite GetSprite(string key)
         {
-            Texture2D tex = GetTexture(key) ?? throw new InvalidOperationException($"Texture '{key}' not found");
-            Rect spriteRect = new Rect(0, 0, tex.width, tex.height);
-            Sprite sprite = Sprite.Create(tex, spriteRect, new Vector2(tex.width / 2, tex.height / 2));
-            sprite.name = tex.name;
-            return sprite;
+            if (SpriteMap.TryGetValue(key, out Sprite sprite))
+                return sprite;
+            else
+                throw new InvalidOperationException($"Sprite '{key}' not found");
         }
 
         internal static void LoadTextureFromFile(string path)
         {
             byte[] textureData = File.ReadAllBytes(path);
-            var texture = new Texture2D(2, 2);
+            var texture = new Texture2D(2, 2, TextureFormat.RGBA32, false, false);
             texture.LoadImage(textureData);
+            texture.anisoLevel = 2;
+            texture.wrapModeU = TextureWrapMode.Clamp;
+            texture.wrapModeV = TextureWrapMode.Clamp;
+            texture.wrapModeW = TextureWrapMode.Repeat;
             texture.DontDestroyOnLoad();
             
             string fileName = Path.GetFileNameWithoutExtension(path);
             texture.name = fileName;
             TextureMap[fileName] = texture;
+
+            Vector2 size = new Vector2(texture.width, texture.height);
+            Rect spriteRect = new Rect(Vector2.zero, size);
+            var sprite = Sprite.Create(texture, spriteRect, Vector2.zero);
+            sprite.DontDestroyOnLoad();
+
+            sprite.name = texture.name;
+            SpriteMap[fileName] = sprite;
         }
     }
 }
