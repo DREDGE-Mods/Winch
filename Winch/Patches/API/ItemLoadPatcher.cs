9d1de2dbfcf0216890b539a6b1c56e4bdbea9253
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-07-17 09:26:07 -0400

    Add dredge sprite

--- Winch/Patches/API/ItemLoadPatcher.cs
+++ Winch/Patches/API/ItemLoadPatcher.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using Winch.Core;
@@ -12,8 +13,18 @@ namespace Winch.Patches.API
     [HarmonyPatch(nameof(ItemManager.OnItemDataAddressablesLoaded))]
     class ItemLoadPatcher
     {
+        public static void ChangeDredgeCrane(IList<ItemData> items)
+        {
+            var dredge1 = items.WhereType<ItemData, DredgeItemData>().FirstOrDefault(dredge => dredge.id == "dredge1");
+            if (dredge1 != null)
+            {
+                dredge1.itemTypeIcon = TextureUtil.GetSprite("DredgeIcon");
+            }
+        }
+
         public static void Prefix(ItemManager __instance, AsyncOperationHandle<IList<ItemData>> handle)
         {
+            ChangeDredgeCrane(handle.Result);
             ItemUtil.AddModdedItemData(handle.Result);
             DredgeEvent.AddressableEvents.ItemsLoaded.Trigger(__instance, handle, true);
         }
