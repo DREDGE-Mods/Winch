093ddfecf8283e0fb1f12560d89ae9ad640e3521
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-08-09 00:32:50 -0400

    Fix test ability

--- Winch.Examples/ExampleItems/TestAbility.cs
+++ Winch.Examples/ExampleItems/TestAbility.cs
@@ -44,17 +44,25 @@ namespace ExampleItems
 
         public static void StabilizeOcean()
         {
+            GameManager.Instance.WaveController.steepness = 0;
             Shader.SetGlobalFloat("_WaveSteepness", 0);
-            Shader.SetGlobalFloat("_WaveLength", 0);
+            GameManager.Instance.WaveController.wavelength = 1;
+            Shader.SetGlobalFloat("_WaveLength", 1);
+            GameManager.Instance.WaveController.speed = 0;
             Shader.SetGlobalFloat("_WaveSpeed", 0);
+            GameManager.Instance.WaveController.waveDirections = new float[4] { 0, 0, 0, 0 };
             Shader.SetGlobalVector("_WaveDirections", Vector4.zero);
         }
 
         public static void UnstabilizeOcean()
         {
+            GameManager.Instance.WaveController.steepness = 0.1158f;
             Shader.SetGlobalFloat("_WaveSteepness", 0.1158f);
+            GameManager.Instance.WaveController.wavelength = 6;
             Shader.SetGlobalFloat("_WaveLength", 6);
+            GameManager.Instance.WaveController.speed = 0.1f;
             Shader.SetGlobalFloat("_WaveSpeed", 0.1f);
+            GameManager.Instance.WaveController.waveDirections = new float[4] { 0.1f, 0.4f, 0.2f, 0.3f };
             Shader.SetGlobalVector("_WaveDirections", new Vector4(0.1f, 0.4f, 0.2f, 0.3f));
         }
 
