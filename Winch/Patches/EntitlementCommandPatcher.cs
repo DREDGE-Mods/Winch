e5ba16b1977ba7129912a335198ce31c6570bb99
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-08-09 12:27:50 -0400

    Remove dlc commands (they don't work anyways)

---
+++ Winch/Patches/EntitlementCommandPatcher.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Winch.Core;
+
+namespace Winch.Patches
+{
+    [HarmonyPatch(typeof(EntitlementManager), nameof(EntitlementManager.AddTerminalCommands))]
+    internal static class EntitlementCommandPatcher
+    {
+        [HarmonyPrefix]
+        public static bool Prefix() {
+            return false;
+        }
+    }
+}
