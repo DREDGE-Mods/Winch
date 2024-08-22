1075a0273320fc79059161b98ea297637d4bfbf3
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-08-22 09:02:23 -0400

    Remove these as dredge has them

--- Winch/Util/WinchExtensions.cs
+++ Winch/Util/WinchExtensions.cs
@@ -1238,26 +1238,6 @@ public static class WinchExtensions
     #endregion
 
     #region RectTransform
-    public static void SetLeft(this RectTransform rt, float left)
-    {
-        rt.offsetMin = new Vector2(left, rt.offsetMin.y);
-    }
-
-    public static void SetRight(this RectTransform rt, float right)
-    {
-        rt.offsetMax = new Vector2(-right, rt.offsetMax.y);
-    }
-
-    public static void SetTop(this RectTransform rt, float top)
-    {
-        rt.offsetMax = new Vector2(rt.offsetMax.x, -top);
-    }
-
-    public static void SetBottom(this RectTransform rt, float bottom)
-    {
-        rt.offsetMin = new Vector2(rt.offsetMin.x, bottom);
-    }
-
     public static void SetWidth(this RectTransform rt, float width)
     {
         rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
