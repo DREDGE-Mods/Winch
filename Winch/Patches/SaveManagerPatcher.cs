3da5469de9c1069784ac1fccbc7e416417a0d688
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-08-25 17:53:08 -0400

    extra

--- Winch/Patches/SaveManagerPatcher.cs
+++ Winch/Patches/SaveManagerPatcher.cs
@@ -6,11 +6,32 @@ namespace Winch.Patches
     [HarmonyPatch(typeof(SaveManager))]
     internal static class SaveManagerPatcher
     {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(SaveManager.Init))]
+        public static void Init(SaveManager __instance)
+        {
+            WinchCore.Log.Debug($"Init()");
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(SaveManager.LoadAllIntoMemory))]
+        public static void LoadAllIntoMemory(SaveManager __instance)
+        {
+            WinchCore.Log.Debug($"LoadAllIntoMemory()");
+        }
+
         [HarmonyPrefix]
         [HarmonyPatch(nameof(SaveManager.SaveGameFile))]
         public static void SaveGameFile(SaveManager __instance, bool useBackupHistory)
         {
             WinchCore.Log.Debug($"SaveGameFile({useBackupHistory})");
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(SaveManager.LoadIntoMemory))]
+        public static void LoadIntoMemory(SaveManager __instance, int slot, ref SaveData __result)
+        {
+            WinchCore.Log.Debug($"LoadIntoMemory({slot}) => {__result}");
         }
 
         [HarmonyPostfix]
@@ -32,7 +53,6 @@ namespace Winch.Patches
         public static void DeleteSaveFile(SaveManager __instance, int slot)
         {
             WinchCore.Log.Debug($"DeleteSaveFile({slot})");
-
         }
     }
 }
