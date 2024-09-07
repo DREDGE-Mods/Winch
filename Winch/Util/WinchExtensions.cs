332af49cabb927c2eb45565ff7a5b920e3c88331
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-09-07 07:04:24 -0400

    DistinctBy

--- Winch/Util/WinchExtensions.cs
+++ Winch/Util/WinchExtensions.cs
@@ -1586,6 +1586,25 @@ public static class WinchExtensions
         value = default(T);
         return false;
     }
+
+    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        => source.DistinctBy(keySelector, EqualityComparer<TKey>.Default);
+
+    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
+    {
+        if (source == null) throw new ArgumentNullException("source");
+        if (keySelector == null) throw new ArgumentNullException("keySelector");
+        if (comparer == null) throw new ArgumentNullException("comparer");
+
+        HashSet<TKey> knownKeys = new HashSet<TKey>(comparer);
+        foreach (TSource element in source)
+        {
+            if (knownKeys.Add(keySelector(element)))
+            {
+                yield return element;
+            }
+        }
+    }
     #endregion
 
     #region Unity
