a210846a6e5a3b26db1ab5ddaceab784f0afad66
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-09-12 00:41:54 -0400

    Add cell count reset

---
+++ Winch/Patches/API/SerializableGridInitPatcher.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System.Linq;
+using UnityEngine;
+
+namespace Winch.Patches.API;
+
+[HarmonyPatch(typeof(SerializableGrid))]
+[HarmonyPatch(nameof(SerializableGrid.Init))]
+internal static class SerializableGridInitPatcher
+{
+    /// <summary>
+    /// Add reset to cell count because for some reason that isn't there, and so vanilla would just increment anytime Init is called.
+    /// </summary>
+    public static void Prefix(SerializableGrid __instance)
+    {
+        __instance.cellCount = 0;
+    }
+}
