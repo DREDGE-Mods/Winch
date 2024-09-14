3bc181a74eeb778da8a10bf99e21cb76e9c79156
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-09-14 15:57:35 -0400

    example quest grid with dialogue

---
+++ Winch.Examples/ExampleItems/ExampleEnums.cs
@@ -0,0 +1,10 @@
+﻿using Winch.Util;
+
+namespace ExampleItems;
+
+[EnumHolder]
+public static class ExampleEnums
+{
+    public static readonly GridKey EXAMPLE_INPUT;
+    public static readonly GridKey EXAMPLE_OUTPUT;
+}
