a9aea992bfbaf01db58c6269418569c14214a0a3
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-09-14 16:04:26 -0400

    adjust items

--- Winch.Examples/ExampleItems/ExampleEnums.cs
+++ Winch.Examples/ExampleItems/ExampleEnums.cs
@@ -5,6 +5,7 @@ namespace ExampleItems;
 [EnumHolder]
 public static class ExampleEnums
 {
+    public static readonly ItemSubtype EXAMPLE;
     public static readonly GridKey EXAMPLE_INPUT;
     public static readonly GridKey EXAMPLE_OUTPUT;
 }
